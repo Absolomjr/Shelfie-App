b1dc047b884efb870d905d482da19089a41c4fbf
Absolom Orianga <absolomjr100@gmail.com>
2026-05-01 17:24:09 +0300

    feat: implement authentication and dashboard features with themed components

--- app/index.jsx
+++ app/index.jsx
@@ -1,25 +1,31 @@
-import { StyleSheet, Text, View, Image } from 'react-native'
-import React from 'react'
+import { StyleSheet } from 'react-native'
+import { Link } from 'expo-router'
 
-// import Logo from '../assets/img/logo_light.png'
+import ThemedView from "../components/ThemedView"
+import ThemedText from "../components/ThemedText"
+import ThemedLogo from "../components/ThemedLogo"
+import Spacer from "../components/Spacer"
 
 const Home = () => {
     return (
-        <View style={styles.container}>
+        <ThemedView style={styles.container}>
+            <ThemedLogo />
+            <Spacer />
 
-            <Image
-                source={require('../assets/img/logo_light.png')}
-                style={styles.img}
-            />
+            <ThemedText style={styles.title} title={true}>The Number 1</ThemedText>
 
-            <Text style={styles.title}>The Number 1</Text>
-
-            <Text style={{ marginTop: 10, marginBottom: 30 }}>
+            <ThemedText style={{ marginTop: 10, marginBottom: 30 }}>
                 Reading List App
-            </Text>
+            </ThemedText>
 
-            
-        </View>
+            <Link href="/login" style={styles.link}>
+                <ThemedText>Login Page</ThemedText>
+            </Link>
+
+            <Link href="/register" style={styles.link}>
+                <ThemedText>Register Page</ThemedText>
+            </Link>
+        </ThemedView>
     )
 }
 
@@ -30,16 +36,16 @@ const styles = StyleSheet.create({
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
-
+    },
+    img: {
+        marginVertical: 20
     },
     title: {
         fontWeight: 'bold',
         fontSize: 18,
-
     },
-    img: {
-        marginVertical: 20,
-
-    },
-    
+    link: {
+        marginVertical: 10,
+        borderBottomWidth: 1
+    }
 })
