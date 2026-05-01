abe8aec4798d9e9c0ab7f75690bfc4e380ac4194
Absolom Orianga <absolomjr100@gmail.com>
2026-05-02 02:18:42 +0300

    feat: implement book management features with context, CRUD operations, and UI updates

--- app/(dashboard)/_layout.jsx
+++ app/(dashboard)/_layout.jsx
@@ -1,25 +1,65 @@
-import { useUser } from '../../hooks/useUser'
-import { useRouter } from 'expo-router'
-import { useEffect } from 'react'
-import { Text } from 'react-native'
+import { Stack, Tabs } from "expo-router"
+import { useColorScheme } from "react-native"
+import { Colors } from "../../constants/Colors"
+import { Ionicons } from "@expo/vector-icons"
 
-const GuestOnly = ({ children }) => {
-    const { user, authChecked } = useUser()
-    const router = useRouter()
+import UserOnly from "../../components/auth/UserOnly"
 
-    useEffect(() => {
-        if (authChecked && user !== null) {
-            router.replace("/profile")
-        }
-    }, [user, authChecked])
+export default function DashboardLayout() {
+    const colorScheme = useColorScheme()
+    const theme = Colors[colorScheme] ?? Colors.light
 
-    if (!authChecked || user) {
-        return (
-            <Text>Loading</Text>
-        )
-    }
-
-    return children
+    return (
+        <UserOnly>
+            <Tabs
+                screenOptions={{
+                    headerShown: false,
+                    tabBarStyle: { backgroundColor: theme.navBackground, paddingTop: 10, height: 90 },
+                    tabBarActiveTintColor: theme.iconColorFocused,
+                    tabBarInactiveTintColor: theme.iconColor,
+                }}
+            >
+                <Tabs.Screen
+                    name="profile"
+                    options={{
+                        title: "Profile", tabBarIcon: ({ focused }) => (
+                            <Ionicons
+                                size={24}
+                                name={focused ? 'person' : 'person-outline'}
+                                color={focused ? theme.iconColorFocused : theme.iconColor}
+                            />
+                        )
+                    }}
+                />
+                <Tabs.Screen
+                    name="books"
+                    options={{
+                        title: "Books", tabBarIcon: ({ focused }) => (
+                            <Ionicons
+                                size={24}
+                                name={focused ? 'book' : 'book-outline'}
+                                color={focused ? theme.iconColorFocused : theme.iconColor}
+                            />
+                        )
+                    }}
+                />
+                <Tabs.Screen
+                    name="create"
+                    options={{
+                        title: "Create", tabBarIcon: ({ focused }) => (
+                            <Ionicons
+                                size={24}
+                                name={focused ? 'create' : 'create-outline'}
+                                color={focused ? theme.iconColorFocused : theme.iconColor}
+                            />
+                        )
+                    }}
+                />
+                <Tabs.Screen
+                    name="books/[id]"
+                    options={{ href: null }}
+                />
+            </Tabs>
+        </UserOnly>
+    )
 }
-
-export default GuestOnly
